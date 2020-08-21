1d6e1f005d9bcb931df87c583fb691f42821b583
Mcl <mixool0204@gmail.com>
2020-08-21 21:50:38 +0800

    Update Tstart.sh

--- Tstart.sh
+++ Tstart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-exec tor &
-exec /usr/local/bin/gost -L socks5+ws://:80 -F=socks5://127.0.0.1:9050?bypass=~*.onion
+/usr/local/bin/gost -L socks5+ws://:80 -F=socks5://127.0.0.1:9050?bypass=~*.onion
+exec tor
